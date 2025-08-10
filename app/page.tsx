531b05fb8c5df901ff93a4c5eeec67100cd196bd
Naresh <tempalltheway@gmail.com>
2025-08-10 19:10:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -791,7 +791,7 @@ export default function Home() {
                    document.getElementById('features')?.scrollIntoView({ behavior: 'smooth' });
                  }}
                >
-                 📚 Learn More About EdgeMind
+                 Learn More About EdgeMind
                </motion.button>
              </div>
            </motion.div>
